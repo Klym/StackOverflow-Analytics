c50912b52c60e95d32dabb925ebc83f3398bfc5d
Maksim Klimenko <klym.programmer@yandex.ru>
2016-11-05 00:08:35 +0300

    add connection to database

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,14 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace StackOverflow_Analytics {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        string connectionStr;
+
         public MainWindow() {
             InitializeComponent();
+            connectionStr = Properties.Settings.Default.stackoverflowConnectionString;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            string sql = "SELECT * FROM users";
+            SqlConnection connection = null;
+            try {
+                connection = new SqlConnection(connectionStr);
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                //MessageBox.Show(reader["display_name"].ToString());
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                if (connection != null)
+                    connection.Close();
+            }
         }
     }
 }
