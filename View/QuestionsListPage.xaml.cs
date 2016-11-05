0a4f0b6f35cb6be79d046ec98ecf4b0114003d1a
Maksim Klimenko <klym.programmer@yandex.ru>
2016-11-06 01:43:08 +0300

    add frame navigate to question page

--- View/QuestionsListPage.xaml.cs
+++ View/QuestionsListPage.xaml.cs
@@ -12,14 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StackOverflow_Analytics.View;
 
 namespace StackOverflow_Analytics {
     /// <summary>
     /// Interaction logic for QuestionsListPage.xaml
     /// </summary>
     public partial class QuestionsListPage : Page {
+
+        private MainWindow mainWindow;
+
         public QuestionsListPage() {
             InitializeComponent();
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) {
+            this.mainWindow = (MainWindow)Window.GetWindow(this);
+        }
+
+        private void questionsList_MouseDoubleClick(object sender, MouseButtonEventArgs e) {            
+            this.mainWindow.MainFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            this.mainWindow.MainFrame.Navigate(new QuestionViewPage());
+        }
+        
     }
 }
