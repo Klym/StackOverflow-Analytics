23a5c75b4bc0ad0c4f9ecb905f7da20e17d7b33c
Maksim Klimenko <klym.programmer@yandex.ru>
2016-11-20 01:05:02 +0300

    create sql query for years tech stats

--- View/TechStats.xaml.cs
+++ View/TechStats.xaml.cs
@@ -12,14 +12,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace StackOverflow_Analytics {
     /// <summary>
     /// Interaction logic for TechStats.xaml
     /// </summary>
     public partial class TechStats : Page {
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
         public TechStats() {
             InitializeComponent();
+
+            SeriesCollection = new SeriesCollection {
+                new LineSeries {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> {4,6,5,2,4 }
+                }/*,
+                new LineSeries {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }*/
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            // modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4, 0 },
+            });
+
+            DataContext = this;
         }
     }
 }
---
+++ sql/select_count_by_years.sql
@@ -0,0 +1,7 @@
+select Count(questions.id), datepart(yyyy, questions.creation_date) as year 
+from q_tags 
+join tags on tag_id = id 
+join questions on questions.id = question_id 
+where name='c#' 
+group by datepart(yyyy, questions.[creation_date])
+order by year
